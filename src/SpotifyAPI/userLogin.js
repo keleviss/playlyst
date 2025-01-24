3d1739a069155b721b3e10f414fcedae3e59ea2e
Viss <kelesidisvissarion@gmail.com>
2025-01-24 23:17:38 +0200

    Update redirect URI for Spotify API authentication to production URL

--- src/SpotifyAPI/userLogin.js
+++ src/SpotifyAPI/userLogin.js
@@ -65,7 +65,7 @@ export const redirectToAuthCodeFlow = async (clientId) => {
   const params = new URLSearchParams();
   params.append("client_id", clientId);
   params.append("response_type", "code");
-  params.append("redirect_uri", "http://localhost:5173/callback");
+  params.append("redirect_uri", "https://playlistjammming.netlify.app/");
   params.append("scope", "user-read-private user-read-email playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public");
   params.append("code_challenge_method", "S256");
   params.append("code_challenge", challenge);
@@ -100,7 +100,7 @@ async function getAccessToken(clientId, code) {
   params.append("client_id", clientId);
   params.append("grant_type", "authorization_code");
   params.append("code", code);
-  params.append("redirect_uri", "http://localhost:5173/callback");
+  params.append("redirect_uri", "https://playlistjammming.netlify.app/");
   params.append("code_verifier", verifier);
 
   const result = await fetch("https://accounts.spotify.com/api/token", {
